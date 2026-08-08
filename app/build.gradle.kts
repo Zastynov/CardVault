a52bf8449b577a91c29dce9005e19bf250b7a0ef
CardVault <cardvault@users.noreply.github.com>
2026-08-08 09:58:40 +0300

    Fix Compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -28,8 +29,10 @@ kotlin {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
+
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-} 
+}
